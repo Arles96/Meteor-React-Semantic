e1aa2054cd495c18143de63f84d65330305c01ad
Arles96 <arles.cerrato@unitec.edu>
2019-03-28 23:50:06 -0600

    Agregando react a meteor

---
+++ client/main.js
@@ -0,0 +1,9 @@
+import './main.html';
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+import App from '../imports/startup/route';
+
+Meteor.startup(() => {
+  render(<App />, document.getElementById('app'));
+});
